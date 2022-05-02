8477ca08f7d5996032d07003d176a3130987d94e
5b6santhosh <5b6santhosh@gmail.com>
2022-05-02 17:39:18 +0530

    add input buttons

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@ export const App = () => {
   let a: string = 'TEST';
   let b: string = 'ETST';
 
+  const box: [] = [];
+
   const onChildClicked = (e: number) => {
     console.warn('callback from parent triggered', e);
   };
@@ -15,11 +17,21 @@ export const App = () => {
     <div className="p-2">
       <h1>React TSX Starter</h1>
       <div>containing:</div>
+
       <ul>
         <li>React</li>
         <li>Twitter Bootstrap</li>
         <li>Basic Functional Components App and ButtonCounter</li>
       </ul>
+
+      <div className="form-group">
+        <input type="text" name="" className="form-control" value="name" />
+      </div>
+
+      <div className="form-group">
+        <input type="button" name="" className="btn btn-primary" value="add" />
+      </div>
+
       <ButtonCounter name={name} onClicked={(e) => onChildClicked(e)} />
     </div>
   );
